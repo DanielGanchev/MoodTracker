4e77e9143ba1d1d323eef137aa4294ff54d9d18d
Daniel Ganchev <dani.ganchev1989@gmail.com>
2025-02-18 13:01:12 +0200

    new files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // Required for static site generation
+  output: 'export',  // This tells Next.js to export static files to 'out' directory
   images: {
     unoptimized: true // Required for static export
   }
